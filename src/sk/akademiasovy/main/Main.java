3f4992c83d1488c97bcdcec849662eac0c8e3356
dominiknovicky <dominik.novicky24@gmail.com>
2018-01-11 12:32:31 +0100

    added triangle

--- src/sk/akademiasovy/main/Main.java
+++ src/sk/akademiasovy/main/Main.java
@@ -2,6 +2,9 @@ package sk.akademiasovy.main;
 
 import sk.akademiasovy.geometry.Rectangle;
 import sk.akademiasovy.geometry.Square;
+import sk.akademiasovy.geometry.Triangle;
+
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,15 +12,21 @@ public class Main {
         Square square1 = new Square(7.5),
                square2 = new Square();
 
-        System.out.println(square1.getArea());
-        System.out.println(square1.getPerimeter());
+        System.out.println("Square area: " +square1.getArea());
+        System.out.println("Square perimeter: " +square1.getPerimeter());
 
         Rectangle rec1 = new Rectangle();
         Rectangle rec2 = new Rectangle(5);
         Rectangle rec3 = new Rectangle(5,10);
-        System.out.println(rec1.getArea());
-        System.out.println(rec2.getPerimeter());
-        System.out.println(rec3.getDiagonal());
+        System.out.println("Rectangle area: " +rec1.getArea());
+        System.out.println("Rectangle perimeter: " +rec2.getPerimeter());
+        System.out.println("Rectangle diagonal: " +rec3.getDiagonal());
 
+        Triangle trn1 = new Triangle(4,8,11),
+                 trn2 = new Triangle();
+        System.out.println("Triangle area: " +trn1.getArea());
+        System.out.println("Triangle perimeter: " +trn1.getPerimeter());
+        System.out.println("Is triangle right-angled? " +trn1.isRightAngled());
+        System.out.println("Is triangle right-angled? " +trn2.isRightAngled());
     }
 }
